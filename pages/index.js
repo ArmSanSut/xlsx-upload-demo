0f875b114bac5fb41ad81ff092b107d096a80511
Inarm Sansutnanont <mramrasansut@outlook.co.th>
2023-08-03 13:11:11 +0700

    fetch data from index.js Page -> render data from client side

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
 import Reat, {useState, useEffect} from 'react';
 import axios from 'axios';
-import XLSX from 'xlsx';
-import XLSXReader from '../src/components/XLSXReader';
+import * as XLSX from 'xlsx';
 
 
 export default function Home() {
 
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+      const fetchAndReadXLSX = async () => {
+  
+        try {
+          const response = await fetch('/assets/ProductCatalogue.xlsx');
+          const data = await response.arrayBuffer();
+          const workbook = XLSX.read(new Uint8Array(data), { type: 'array' });
+
+          console.log('workbook', workbook)
+
+          const sheetName = workbook.SheetNames[0];
+          const sheetData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+         
+          setData(sheetData);
+          setCategory(workbook.Sheets)
+        } catch (error) {
+          console.error('Error reading XLSX file:', error.message);
+        }
+      };
+  
+      fetchAndReadXLSX();
+  }, []);
+  console.log('data',data)
+
   return (
     <div>
-      <h2>Table of Contents</h2>
-      <XLSXReader/>
     </div>
   )
 }
